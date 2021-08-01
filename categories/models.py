96ebff8d65b758381e1ec1de32609dc14fa66c8c
Kajetan Maślak <kajtek.maslak@icloud.com>
2021-08-01 21:57:24 +0200

    Created model for categories app

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, blank=False, null=False)
+    icon = models.CharField(max_length=128, blank=False, null=False)
+    description = models.TextField(default='', blank=False, null=False)
+    slug = models.SlugField(null=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+
+        return super(Category, self).save(*args, **kwargs)
