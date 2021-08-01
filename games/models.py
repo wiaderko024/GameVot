16cf53760dd5fa06a19f92f16b039c8370506826
Kajetan Maślak <kajtek.maslak@icloud.com>
2021-08-01 21:34:08 +0200

    Added cover (imagefield) to game models

--- games/models.py
+++ games/models.py
@@ -6,6 +6,7 @@ class Game(models.Model):
     title = models.CharField(max_length=300, blank=False, null=False, unique=True)
     year = models.IntegerField(default=2000, blank=False, null=False)
     description = models.TextField(default="", blank=True, null=True)
+    cover = models.ImageField(upload_to='game_covers', blank=True, null=True)
     slug = models.SlugField(null=True, editable=False)
 
     def __str__(self):
