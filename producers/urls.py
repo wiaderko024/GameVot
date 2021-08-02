7ad80b2d0579dab5a089ec0ca9279edd56b62da9
Kajetan Maślak <kajtek.maslak@icloud.com>
2021-08-02 16:25:38 +0200

    Created basic view and template for producer page

--- producers/urls.py
+++ producers/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import all_producers
+from .views import all_producers, producer_page
 
 
 app_name = 'producers'
 
 urlpatterns = [
-    path('all-producers', all_producers, name='all_producers_page'),
+    path('all-producers/', all_producers, name='all_producers_page'),
+    path('<slug:producer_slug>/', producer_page, name='producer_page'),
 ]
--- producers/views.py
+++ producers/views.py
@@ -10,3 +10,13 @@ def all_producers(request):
     }
 
     return render(request, 'all_producers.html', context=context)
+
+
+def producer_page(request, producer_slug):
+    producer = Producer.objects.get(slug=producer_slug)
+
+    context = {
+        'producer': producer,
+    }
+
+    return render(request, 'producer.html', context=context)
