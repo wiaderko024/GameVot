92e9fe71ecb928ef214be45017376281d4232136
Kajetan Maślak <kajtek.maslak@icloud.com>
2021-08-02 17:00:34 +0200

    Added gettings categories at navbar from backend

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from categories.models import Category
 
 
 def home_page(request):
-    return render(request, 'index.html')
+    categories = Category.objects.all()
+
+    context = {
+        'categories': categories,
+    }
+
+    return render(request, 'index.html', context=context)
