6e967917d9a79bff00c59fb4bfcfdfbc193704d1
Kajetan Maślak <kajtek.maslak@icloud.com>
2021-08-02 17:04:19 +0200

    Added categories at navbar at all pages

--- games/views.py
+++ games/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .models import Game
+from categories.models import Category
 
 
 def all_games(request):
     games = Game.objects.all()
+    categories = Category.objects.all()
 
     context = {
         'games': games,
+        'categories': categories,
     }
 
     return render(request, 'all_games.html', context=context)
@@ -14,9 +17,11 @@ def all_games(request):
 
 def game_page(request, game_slug):
     game = Game.objects.get(slug=game_slug)
+    categories = Category.objects.all()
 
     context = {
         'game': game,
+        'categories': categories,
     }
 
     return render(request, 'game.html', context=context)
--- producers/views.py
+++ producers/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .models import Producer
+from categories.models import Category
 
 
 def all_producers(request):
     producers = Producer.objects.all()
+    categories = Category.objects.all()
 
     context = {
         'producers': producers,
+        'categories': categories,
     }
 
     return render(request, 'all_producers.html', context=context)
@@ -14,9 +17,11 @@ def all_producers(request):
 
 def producer_page(request, producer_slug):
     producer = Producer.objects.get(slug=producer_slug)
+    categories = Category.objects.all()
 
     context = {
         'producer': producer,
+        'categories': categories,
     }
 
     return render(request, 'producer.html', context=context)
