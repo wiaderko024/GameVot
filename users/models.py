4a2defddd1ad9ad69636abab504fc65afdc95954
Kajetan Maślak <kajtek.maslak@icloud.com>
2021-09-08 13:58:21 +0200

    Created basic model for user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    avatar = models.ImageField(upload_to='avatars', blank=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now, editable=False, null=False)
+
+    def __str__(self):
+        return self.user.username
+
+    def save(self, *args, **kwargs):
+        if not self.created_at:
+            self.created_at = timezone.now()
+
+        return super(Profile, self).save(*args, **kwargs)
