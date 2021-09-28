f777ebeecd8d23f5fea0b79172b577109eae8a01
Kajetan Maślak <kajtek.maslak@icloud.com>
2021-09-29 01:25:55 +0200

    Connected with PostgreSQL Database

--- gamevot/settings.py
+++ gamevot/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'gamevot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gamevot',
+        'USER': 'postgres',
+        'PASSWORD': 'admin1',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
